13aa355dd2c62c4c1c8356f4ab8c1c4d59d28bab
Juan Daniel <dani3l25948@gmail.com>
2022-03-02 23:14:35 -0600

    arreglo en jenkins

--- frontend/src/tests/transferencia.test.js
+++ frontend/src/tests/transferencia.test.js
@@ -110,7 +110,7 @@ describe("Test in <Transferencia />", () => {
     test("should the success call api of TransferenciaSaldo", (done) => {
         reportService.transfer({
             AccountOrigin: "0664422022224713",
-            AccountDestination: "0876422022472227",
+            AccountDestination: "08764220224722272",
             montoTransferir: parseInt("1", 10),
         }).then((res) => {
             expect("ok").toContain(res.msg);
